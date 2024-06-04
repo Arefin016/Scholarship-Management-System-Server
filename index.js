37bb0e344c1b042c32dfa5c63ea6e8989ec79154
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-06-04 22:01:28 +0600

    Specific data has been loaded

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-const port = process.env.PORT || 5000;
-
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
+require("dotenv").config()
+const port = process.env.PORT || 5000
 
 //middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrdcqgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrdcqgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,28 +17,59 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-});
+  },
+})
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect()
+
+    const topScholarshipCollection = client
+      .db("scholarshipDb")
+      .collection("topScholarship")
+
+    app.get("/topScholarship", async (req, res) => {
+      const result = await topScholarshipCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get("/topScholarship/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: {
+          universityName: 1,
+          universityImage: 1,
+          scholarshipCategory: 1,
+          applicationDeadline: 1,
+          subjectCategory: 1,
+          applicationFees: 1,
+          scholarshipDescription: 1,
+          _id: 1,
+          universityLocation: 1,
+        },
+      }
+      const result = await topScholarshipCollection.findOne(query, options)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 })
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    )
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.dir)
 
-
-app.get('/', (req, res) => {
-    res.send('Scholarship Management System is running');
+app.get("/", (req, res) => {
+  res.send("Scholarship Management System is running")
 })
 
 app.listen(port, () => {
-    console.log(`Scholarship Management System is running on port ${port}`);
+  console.log(`Scholarship Management System is running on port ${port}`)
 })
