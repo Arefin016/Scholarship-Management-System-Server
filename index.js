cf7135a8a2b968d4d28e8e59f199813d30fd8214
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-06-06 10:58:34 +0600

    Make admin has been done

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     const userCollection = client.db("scholarshipDb").collection("users")
 
     //User related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body
       //insert email if user does not exits:
@@ -46,6 +51,38 @@ async function run() {
       res.send(result)
     })
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.patch("/users/moderator/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: "moderator",
+        },
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //Top Scholarship Related
     app.get("/topScholarship", async (req, res) => {
       const result = await topScholarshipCollection.find().toArray()
       res.send(result)
